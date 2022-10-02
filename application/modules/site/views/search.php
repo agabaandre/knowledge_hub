bf59885ccac303460ea97b8bc5fb56fbe0afcad4
AGABA ANDREW <agabaandre@gmail.com>
2022-10-03 00:44:06 +0300

    search size

--- application/modules/site/views/search.php
+++ application/modules/site/views/search.php
@@ -1,19 +1,19 @@
 <div class="row">
 
-		     <div class="card col-lg-12">
-		     	<div class="card-header">
-		     		<h4>Search</h4>
-		     	</div>
-	           <div class="card-body text-left">
-				<div class="form-group">
-					<input type="text" class="form-control" name="" placeholder="Search here">
-				</div>
-			 	</div>
+	<div class="card col-lg-12">
+		<div class="card-header">
+			<h4>Search</h4>
+		</div>
+		<div class="card-body text-left">
+			<div class="form-group">
+				<input type="text" class="form-control" name="" placeholder="Search here" style="padding: 15px; font-size:20px;">
+			</div>
+		</div>
 
-			 	<!-- <div class="row text-center p-10">
+		<!-- <div class="row text-center p-10">
 			 		<i class="fa fa-info-circle text-muted fa-5x"></i>
 			 		< No Records
 			 	</div> -->
-			</div>
-	
+	</div>
+
 </div>
