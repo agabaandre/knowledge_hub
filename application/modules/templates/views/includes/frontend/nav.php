889329b4abdd6362e93635f86d74402ed8bb5703
AGABA ANDREW <agabaandre@gmail.com>
2022-10-08 22:16:32 +0300

    untracked node

---
+++ application/modules/templates/views/includes/frontend/nav.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background:#348f41; color:#FFF; border-bottom: 1px solid #6B4C24; margin-bottom:-20px;">
+    <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo base_url() ?>site" style="color:#FFFFFF !important;">Home <span class=" sr-only"></span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
--- application/modules/templates/views/plain.php
+++ application/modules/templates/views/plain.php
@@ -9,56 +9,56 @@ require_once("includes/frontend/preloader.php");
 ?>
 
 
-<div class="row p-4 pt-2 align-items-center header-bg"
- style=" background-image: url(<?php echo base_url(); ?>assets/images/map.png);">
+<div class="row p-4 pt-2 align-items-center header-bg" style=" background-image: url(<?php echo base_url(); ?>assets/images/map.png);">
     <div class="col-md-1 col-lg-1 col-sm-12 text-center">
-        <img style="filter: brightness(0) invert(1);" src="<?php echo base_url(); ?>assets/images/icon_Africa_cdc.png" width="250px">
+        <a class="nav-link" href="<?php echo base_url() ?>site" style="color:#FFFFFF !important;"><img style="filter: brightness(0) invert(1);" src="<?php echo base_url(); ?>assets/images/icon_Africa_cdc.png" width="250px"></a>
     </div>
-    <div class="col-md-11 col-lg-11 col-sm-12 text-center">
-        <h3 class="text-white">Africa CDC RCC Health Security Knowledge Hub</h3>
-        <h5  style="color:#C3A366; padding-bottom: 20px;">GLOBAL AND REGIONAL POLICIES, GUIDANCE, DATABASES AND RESOURCES</h5>
+    <div class="col-md-10 col-lg-10 col-sm-12 text-center">
+        <h3 class="text-white" style="font-size:25px;">Africa CDC RCC Health Security Knowledge Hub</h3>
+        <h5 style="color:#C3A366; padding-bottom: 20px;">Global and Regional Policies, Guidance, Databases and Resources</h5>
 
-        
     </div>
+    <div class="col-md-1 col-lg-1 col-sm-12 text-center">
+    </div>
+
 </div>
 
 <div class="container p-3" style="margin-top: -50px;">
 
- <?php
-    $this->load->view($module . "/" . $view); 
-  ?>
+    <?php
+    $this->load->view($module . "/" . $view);
+    ?>
 
     <!-- [ Main Content ] end -->
-                       
+
 </div>
-    <!-- [ Main Content ] end -->
+<!-- [ Main Content ] end -->
 
-    <!-- Required Js -->
-    <script src="<?php echo base_url(); ?>assets/js/vendor-all.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/pcoded.min.js"></script>
-    <!-- prism Js -->
-    <script src="<?php echo base_url(); ?>assets/plugins/prism/js/prism.min.js"></script>
-  
+<!-- Required Js -->
+<script src="<?php echo base_url(); ?>assets/js/vendor-all.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/pcoded.min.js"></script>
+<!-- prism Js -->
+<script src="<?php echo base_url(); ?>assets/plugins/prism/js/prism.min.js"></script>
 
-    <div class="footer-fab">
-        <div class="b-bg">
-            <i class="fas fa-info"></i>
-        </div>
-        <div class="fab-hover">
-            <ul class="list-unstyled">
-                <li><a href="<?php echo base_url(); ?>"  data-text="FAQs" class="btn btn-icon btn-rounded btn-success m-0"><i class="feather icon-list"></i></a>
-                </li>
-                <li><a href="<?php echo base_url(); ?>"  
-                    data-text="CDC Website" class="btn btn-icon btn-rounded btn-primary m-0">
+
+<div class="footer-fab">
+    <div class="b-bg">
+        <i class="fas fa-info"></i>
+    </div>
+    <div class="fab-hover">
+        <ul class="list-unstyled">
+            <li><a href="<?php echo base_url(); ?>" data-text="FAQs" class="btn btn-icon btn-rounded btn-success m-0"><i class="feather icon-list"></i></a>
+            </li>
+            <li><a href="<?php echo base_url(); ?>" data-text="CDC Website" class="btn btn-icon btn-rounded btn-primary m-0">
                     <i class="feather icon feather icon-globe"></i>
                 </a>
-                </li>
-            </ul>
-        </div>
+            </li>
+        </ul>
     </div>
+</div>
 
-    <script src="<?php echo base_url(); ?>assets/js/analytics.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/analytics.js"></script>
 </body>
 
 </html>
