f0082d0309881d189dd036ed2f484feddcc1fccd
AGABA ANDREW <agabaandre@gmail.com>
2022-10-19 23:47:19 +0300

    rcc dashboards

---
+++ application/modules/rccs/models/Rccs_mdl.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard_mdl extends CI_Model
+{
+
+
+    public function __Construct()
+    {
+
+        parent::__Construct();
+    }
+    public function getData()
+    {
+
+        //
+
+    }
+}
