c66cc78f69ef4433e9df19614ffb1543cc44f01a
Henry Nkuke <henry.nkuke@interswitchng.com>
2022-10-22 12:24:58 +0300

    Setup dashboard charts

---
+++ application/modules/rccs/views/partials/donut_charts.php
@@ -0,0 +1,78 @@
+
+<div class="col-md-3">
+
+ <div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-primary">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-danger">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-danger">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-info">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-primary">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header justify-content-center">
+            <h5>Radial with Images</h5>
+        </div>
+        <div class="card-body justify-content-center">
+            <div data-label="50%" class="radial-bar radial-bar-50 radial-bar-lg radial-bar-primary">
+            </div>
+        </div>
+    </div>
+</div>
+</div>
---
+++ application/modules/rccs/views/partials/large_charts.php
@@ -0,0 +1,26 @@
+
+<div class="row">
+<!-- [ bar-Chart ] start -->
+<div class="col-xl-12">
+    <div class="card">
+        <div class="card-header">
+            <h5>Bar [ Basic ] Chart</h5>
+        </div>
+        <div class="card-body">
+            <div id="chart-highchart-bar1" style="width: 100%; height: 450px;"></div>
+        </div>
+    </div>
+</div>
+<!-- [ bar-Chart ] end -->
+<!-- [ line-Chart ] start -->
+<div class="col-xl-12">
+    <div class="card">
+        <div class="card-header">
+            <h5>Line [ Basic ] Chart</h5>
+        </div>
+        <div class="card-body">
+            <div id="chart-highchart-line1" style="width: 100%; height: 350px;"></div>
+        </div>
+    </div>
+</div>
+</div>
