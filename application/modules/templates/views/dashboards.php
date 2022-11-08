8723bfa3d41572cfe8de25fd529ac437eee078b9
AGABA ANDREW <agabaandre@gmail.com>
2022-11-08 03:19:17 +0300

    auth

--- application/modules/templates/views/dashboards.php
+++ application/modules/templates/views/dashboards.php
@@ -48,7 +48,7 @@ require_once("includes/header.php");
             <a class="nav-item nav-link active" href="<?php echo base_url(); ?>rccs">Home</a>
             <a class="nav-item nav-link" href="<?php echo base_url(); ?>rccs/kpi_comparison">By KPI</a>
             <a class="nav-item nav-link" href="<?php echo base_url(); ?>rccs/country_comparison">By Member State</a>
-            <a class="nav-item nav-link" href="<?php echo base_url(); ?>admin">Administration</a>
+            <a class="nav-item nav-link" href="<?php echo base_url(); ?>dashboard">Administration</a>
             <a class="nav-item nav-link" href="<?php echo base_url(); ?>auth/logout_rcc">Sign Out</a>
         </div>
     </div>
