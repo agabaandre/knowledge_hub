b092a5152bc571b53ca2bf2eb63f12f1da0acd1f
Agaba Andrew <agabaandre@gmail.com>
2022-11-23 00:21:00 +0300

    quiz

--- application/modules/templates/views/main.php
+++ application/modules/templates/views/main.php
@@ -1,37 +1,38 @@
-<?php require_once('includes/header.php');
-      require_once('includes/loader.php'); 
-  ?>
-		<!-- Page -->
-		<div class="page">
-			<?php require("includes/top_bar_main.php");
-                  require("includes/top_bar_mobile.php");
-                  require("includes/nav.php");
-            ?>
-			<!-- Main-content -->
-			<div class="main-content horizontal-content">
+<?php
+require_once('includes/header.php');
+require_once('includes/preloader.php');
+?>
+<!-- Page -->
+<div class="page">
+	<?php
+	require("includes/top_bar_main.php");
+	require("includes/top_bar_mobile.php");
+	require("includes/nav.php");
+	?>
+	<!-- Main-content -->
+	<div class="main-content horizontal-content">
 
-				<!-- Container-fluid -->
-				<div class="container">
+		<!-- Container-fluid -->
+		<div class="container">
 
-					<!-- Main-content-body -->
-					<div class="">
+			<!-- Main-content-body -->
+			<div class="">
 
-						<!-- header-title -->
-						<div class="header-title">
-							<div class="mb-0 mb-lg-0 mb-xl-0">
-								<h4 class="mb-2">Dashboard</h4>
-								<div class="main-content-breadcrumb"> <span>Dashboard</span> <span><?php echo $title ?></span> </div>
-							</div>
-						</div>
-		
-                        <?php   $this->load->view($module . "/" . $view); ?> ?>
+				<!-- header-title -->
+				<div class="header-title">
+					<div class="mb-0 mb-lg-0 mb-xl-0">
+						<h4 class="mb-2">Dashboard</h4>
+						<div class="main-content-breadcrumb"> <span>Dashboard</span> <span><?php echo $title ?></span> </div>
 					</div>
-					<!-- Main-content-body closed -->
 				</div>
-				<!-- Container-fluid closed -->
-			</div>
-			<!-- Main-content closed -->
-		</div>
-		<!-- Page closed -->
-        
 
+				<?php $this->load->view($module . "/" . $view); ?>
+			</div>
+			<!-- Main-content-body closed -->
+		</div>
+		<!-- Container-fluid closed -->
+	</div>
+	<!-- Main-content closed -->
+</div>
+<!-- Page closed -->
+<?php require("includes/footer.php"); ?>
