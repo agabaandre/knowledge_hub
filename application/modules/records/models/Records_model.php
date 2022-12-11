ba17cec5e913f21ebcb9cf1bdb7765c2384e374a
Agaba Andrew <agabaandre@gmail.com>
2022-12-11 23:19:20 +0300

    dashboard

--- application/modules/records/models/Records_model.php
+++ application/modules/records/models/Records_model.php
@@ -1,21 +1,24 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Records_model extends CI_Model {
+class Records_model extends CI_Model
+{
 
-	
-	public function __construct(){
+
+	public function __construct()
+	{
 
 		parent::__construct();
 	}
 
 
-	public function search($term,$type,$limit=null,$start=0){
+	public function search($term, $type, $limit = null, $start = 0)
+	{
 
-		$this->applyFilter($term,$type);
+		$this->applyFilter($term, $type);
 
-		if($limit){
-			$this->db->limit($limit,$start);
+		if ($limit) {
+			$this->db->limit($limit, $start);
 		}
 
 		$results = $this->db->get('publication')->result();
@@ -23,74 +26,79 @@ class Records_model extends CI_Model {
 		foreach ($results as $key => $pub) {
 			$results[$key] = $this->publicationsmodel->attach_related($pub);
 		}
-		 
+
 		return $results;
 	}
 
-	public function count($term,$type){	
-		$this->applyFilter($term,$type);
+	public function count($term, $type)
+	{
+		$this->applyFilter($term, $type);
 		return count($this->db->get('publication')->result());
 	}
 
-	public function applyFilter($term,$type){
+	public function applyFilter($term, $type)
+	{
 
-		if($type)
-		$this->db->where('file_type_id',$type);
+		if ($type)
+			$this->db->where('file_type_id', $type);
 
-	   if($term):
+		if ($term) :
 
-		    $search = ['search_key'=>$term];
+			$search = ['search_key' => $term];
 
 			$authors    = $this->get_authors($search);
 			$sub_themes = $this->get_subthemes($search);
 
-			$this->db->like('title',$term);
+			$this->db->like('title', $term);
 			$this->db->or_like('description', $term);
 			//if tag, include pubs with the tag
 			$this->db->or_where_in('id', $this->publication_tags($term));
 
-			if(count($authors)>0)
-			$this->db->or_where_in('author_id ',array_values($authors));
-		
-		    if(count($sub_themes)>0)
-			$this->db->or_where_in('sub_thematic_area_id',array_values($sub_themes));
+			if (count($authors) > 0)
+				$this->db->or_where_in('author_id ', array_values($authors));
+
+			if (count($sub_themes) > 0)
+				$this->db->or_where_in('sub_thematic_area_id', array_values($sub_themes));
 		endif;
 	}
 
 
-	public function get_authors($search){
+	public function get_authors($search)
+	{
 
-	  	$authors      = $this->authorsmodel->get($search);
-	  	$author_ids   = [];
+		$authors      = $this->authorsmodel->get($search);
+		$author_ids   = [];
 
 		foreach ($authors as $row) {
-	  		array_push($author_ids, $row->id);
-	  	}
+			array_push($author_ids, $row->id);
+		}
 
-	  	return $author_ids;
+		return $author_ids;
 	}
 
-	public function get_subthemes($search){
+	public function get_subthemes($search)
+	{
 
-	  	$themes       = $this->healththemesmodel->get($search);
-	  	$subthemes_ids = [];
+		$themes       = $this->healththemesmodel->get($search);
+		$subthemes_ids = [];
 
-		  foreach ($themes as $row):
+		foreach ($themes as $row) :
 
-		  		$filter = ['thematic_area_id'=>$row->id];
-		  	    $subthemes       = $this->subthemesmodel->get($filter);
+			$filter = ['thematic_area_id' => $row->id];
+			$subthemes       = $this->subthemesmodel->get($filter);
 
-		  	    //subthemes
-		  	    foreach ($subthemes as $sub) {
-		  	    	array_push($subthemes_ids, $sub->id);
-		  	    }
+			//subthemes
+			foreach ($subthemes as $sub) {
+				array_push($subthemes_ids, $sub->id);
+			}
 
-		  endforeach;
+		endforeach;
 
 		return $subthemes_ids;
 	}
 
-	public function get_questions(){
+	public function get_questions()
+	{
 
 		$result = [];
 
@@ -99,46 +107,46 @@ class Records_model extends CI_Model {
 		foreach ($questions as $question) {
 
 			$answers    = $this->get_answers($question->id);
-			
+
 			$answer_str = array_column($answers, 'answer_text');
 
 			$result[] = array(
-				"question"=> $question->question_text,
+				"question" => $question->question_text,
 				"choices" => $answer_str,
-				"correctAnswer"=>intval($this->get_correct_answer($answers))
+				"correctAnswer" => intval($this->get_correct_answer($answers))
 			);
+		}
 
-	    }
-
-	    return $result;
-
+		return $result;
 	}
 
-	public function get_correct_answer($answers){
+	public function get_correct_answer($answers)
+	{
 
-			foreach ($answers as $key => $value) {
-				
-				if($value['is_correct']==1)
-					return $key;
-			}
+		foreach ($answers as $key => $value) {
+
+			if ($value['is_correct'] == 1)
+				return $key;
+		}
 	}
 
-	public function get_answers($id){
+	public function get_answers($id)
+	{
 
-		$this->db->where('question_id',$id);
+		$this->db->where('question_id', $id);
 		return $this->db->get("answers")->result_array();
 	}
 
-	public function publication_tags($tag_text){
+	public function publication_tags($tag_text)
+	{
 		$qry = $this->db->query("SELECT publication_id from publication_tags where tag_id in (SELECT id from tags WHERE  tag_text like '$tag_text%')");
 		$result = $qry->result();
-		$arr=[];
-		
-		foreach($result as $row){
+		$arr = [];
+
+		foreach ($result as $row) {
 			$arr[] = $row->publication_id;
 		}
 
 		return $arr;
 	}
-
 }
