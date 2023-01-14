86c3f2b239fb5df36255c789b534d9382659c06f
Paul <paulobunga.one@gmail.com>
2023-01-14 17:36:49 +0300

    Updated Code: Recentred Admin SignIn Page

--- application/modules/filetypes/views/includes/create-modal.php
+++ application/modules/filetypes/views/includes/create-modal.php
@@ -4,7 +4,9 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="myExtraLargeModalLabel">Create File Type </h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
       <?php echo form_open_multipart(base_url('filetypes/save'), array('id' => 'filetypes', 'class' => 'filetypes')); ?>
 
