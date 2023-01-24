c977de80d52abd1c669dd9e33dc04a0aefcc14d7
AGABA ANDREW <agabaandre@gmail.com>
2023-01-24 19:31:08 +0300

    crossite forgery fix

--- application/modules/templates/views/includes/header.php
+++ application/modules/templates/views/includes/header.php
@@ -6,7 +6,7 @@
 	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="description" content="<?php echo setting()->site_description; ?>" />
-	<meta name="keywords" content="<?php echo setting()->seo_keywords; ?>">
+	<meta name="robots" content="noindex">
 	<meta name="author" content="Africa CDC" />
 	<!-- Title -->
 	<title><?php echo $title; ?></title>
