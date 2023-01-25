0e3a510aff7fb929ef2f17a418ec0860633e8908
AGABA ANDREW <agabaandre@gmail.com>
2023-01-25 09:22:08 +0300

    langauge delay fix

--- application/helpers/langauge.php
+++ application/helpers/langauge.php
@@ -1,13 +1,13 @@
  <div id="google_translate_element" style="display: none;"></div>
-    <select class="selectpicker" data-width="fit" onchange="translateLanguage(this.value);" style="border:#FFF;" com>
-        <option data-content='English' value="English"><span class="flag-icon flag-icon-us"></span>English</option>
-        <option data-content='<span class="flag-icon flag-icon-ar"></span> Arabic' value="Arabic">Arabic</option>
+    <select class="selectpicker" onchange="translateLanguage(this.value);" style="border:#FFF;">
+        <option value="English">English</option>
+        <option value="Arabic">Arabic</option>
         
-        <option data-content='<span class="flag-icon flag-icon-fr"></span> French' value="French">French</option>
-        <option data-content='<span class="flag-icon flag-icon-pt"></span> Portuguese' value="Portuguese">Portuguese
+        <option value="French">French</option>
+        <option  value="Portuguese">Portuguese
         </option>
-        <option data-content='<span class="flag-icon flag-icon-pt"></span> Spanish' value="Spanish">Spanish</option>
-          <option data-content='<span class="flag-icon flag-icon-pt"></span> Swahili' value="Swahili">Swahili</option>
+        <option value="Spanish">Spanish</option>
+        <option value="Swahili">Swahili</option>
 
 
 </select>
