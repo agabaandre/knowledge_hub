23428f748118221182cec8d347fc847ba2a4ef9f
AGABA ANDREW <agabaandre@gmail.com>
2023-01-25 10:56:11 +0300

    tn fix

--- application/modules/auth/views/login/login.php
+++ application/modules/auth/views/login/login.php
@@ -7,9 +7,11 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="description" content="Flash Able Bootstrap admin template made using Bootstrap 4 and it has huge amount of ready made feature, UI components, pages which completely fulfills any dashboard needs." />
-  <meta name="keywords" content="admin templates, bootstrap admin templates, bootstrap 4, dashboard, dashboard templets, sass admin templets, html admin templates, responsive, bootstrap admin templates free download,premium bootstrap admin templates, Flash Able, Flash Able bootstrap admin template">
-  <meta name="author" content="Codedthemes" />
+  <<meta name="description" content="<?php echo setting()->site_description; ?>" />
+  <meta name="robots" content="noindex">
+  <meta name="author" content="Africa CDC" />
+  	<!-- Title -->
+	<title><?php echo $title; ?></title>
 
   <!-- Favicon icon -->
   <link rel="icon" href="<?php echo base_url(); ?>assets/images/icon_Africa_cdc.png" type="image/x-icon">
