b4e4efd4622c109fc764e007b4f845ba888e5035
Paul Obunga <paulobunga.one@gmail.com>
2023-06-02 20:17:41 +0300

    Added a blank RCC Dashboard
    Fixes issue #36

--- resources/views/admin/dashboard/rcc.blade.php
+++ resources/views/admin/dashboard/rcc.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="container-fluid">
-
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
