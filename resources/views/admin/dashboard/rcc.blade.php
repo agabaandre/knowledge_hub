ab054183050325e7dc6cf91ad7ea1ac8570210aa
Paul Obunga <paulobunga.one@gmail.com>
2023-06-02 20:19:24 +0300

    Fixed 404 Issue RCC Dashboard, Fixes #36

--- resources/views/admin/dashboard/rcc.blade.php
+++ resources/views/admin/dashboard/rcc.blade.php
@@ -5,7 +5,11 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">RCC Dashboard</h3>
+                    </div>
                     <div class="card-body">
+
                     </div>
                 </div>
             </div>
