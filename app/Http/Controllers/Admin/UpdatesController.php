fe2da703137a35239c16223e546ff6be5dcdb513
Agaba Andre <agabaandre@gmail.com>
2023-06-05 13:38:54 +0200

    Update UpdatesController.php

--- app/Http/Controllers/Admin/UpdatesController.php
+++ app/Http/Controllers/Admin/UpdatesController.php
@@ -1,1 +1,23 @@
 
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UpdatesController extends Controller
+{
+
+    public function __construct()
+    {
+      
+    }
+
+    public function index(){
+          
+        $output['git pull] = shell_exec('sudo git pull -X theirs');
+        $output['migration] = shell_exec('php artisan migrate');
+         echo "<pre>$output</pre>";
+       return view('admin.updates.index',$output);
+    }
