07f0e31c4e422508b0a774ec7e1573c10c6f4546
Nkuke Henry <henricsanyu@gmail.com>
2023-09-07 10:45:59 +0300

    tag fix

--- resources/views/home/partials/tags.blade.php
+++ resources/views/home/partials/tags.blade.php
@@ -2,12 +2,15 @@
 <div class="d-block mb-2 mt-3">
     <span class="text-success">Tags:</span>
 
+    <div class="row">
     @php 
      $colors = ['dark','blue','green'];
     @endphp
 
     @foreach($tags as $tag)
-        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded">{{ $tag->tag_text }}</a>
+        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 mr-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded col-lg-3 text-center">{{ truncate($tag->tag_text,50) }}</a>
     @endforeach
+
+    </div>
 
 </div>
