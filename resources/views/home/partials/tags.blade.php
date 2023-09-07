9590ea2647c96d23f463cf198925820b5d4eaa42
Nkuke Henry <henricsanyu@gmail.com>
2023-09-07 10:53:14 +0300

    tag fix

--- resources/views/home/partials/tags.blade.php
+++ resources/views/home/partials/tags.blade.php
@@ -8,7 +8,7 @@
     @endphp
 
     @foreach($tags as $tag)
-        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 mr-1 mt-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded col-lg-3 text-center">{{ truncate($tag->tag_text,50) }}</a>
+        <a href="{{ url('records')}}?tag={{$tag->tag_text}}" class="px-3 py-1 mr-1 mt-1 medium bg-{{ $colors[mt_rand(0,2)] }} text-white rounded col-lg-3 text-center">{{ truncate($tag->tag_text,20) }}</a>
     @endforeach
 
     </div>
