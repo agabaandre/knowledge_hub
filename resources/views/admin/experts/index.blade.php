0a5ecf519fd92223014000e4b005f81106a477ac
Paul Obunga <paulobunga.one@gmail.com>
2024-04-12 11:28:32 +0300

    Updated Code

--- resources/views/admin/experts/index.blade.php
+++ resources/views/admin/experts/index.blade.php
@@ -119,6 +119,12 @@
 
 								@endforeach
 							</div>
+
+							<div class="row">
+								<div class="col-lg-12 col-md-12 col-sm-12">
+									{{ $experts->links() }}
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
