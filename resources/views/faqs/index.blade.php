1e866b1053a519d4c2a81bb91cefd1640aba073c
Agaba Andrew <agabaandre@gmail.com>
2024-04-17 19:38:36 +0300

    top bar fix

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -40,7 +40,7 @@
 						<?php $i=1;?>
 						@foreach($faqs as $row)
 						<div class="job_grid d-block border rounded px-3 pt-3 pb-2" >
-							<div class="jb-list01 pl-2" style="border-left: 2px solid green;">
+							<div class="jb-list01 pl-2" style="border-left: 2px var(--theme-color-primary);">
 								<div class="jb-list-01-title">
 									<h5 class="ft-medium mb-1"> {{$i++}}. {{$row->question }}</h5>
 									<p class="pl-2">
