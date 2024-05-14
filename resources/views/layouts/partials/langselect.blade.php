fb91b64e4466c6409791cde9f7c204c83d0b4dfb
Agaba Andrew <agabaandre@gmail.com>
2024-05-14 22:23:28 +0300

    translate

--- resources/views/layouts/partials/langselect.blade.php
+++ resources/views/layouts/partials/langselect.blade.php
@@ -16,26 +16,26 @@
       <ul id="menu-language-menu" class="language-menu">
 <b>Langauges:</b>
         <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('en');return false;" title="English" class="link notranslate">English</a>
+    <a href="#" onclick="doGTranslate('en'); return false;" title="English" class="link notranslate">English</a>
 </li>
 |
 <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('fr');return false;" title="French" class="link notranslate">French </a>
+    <a href="#" onclick="doGTranslate('fr'); return false;" title="French" class="link notranslate">French </a>
 </li>|
 <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('ar');return false;" title="Arabic" class="link notranslate">Arabic</a>
+    <a href="#" onclick="doGTranslate('ar'); return false;" title="Arabic" class="link notranslate">Arabic</a>
 </li>
 |
 <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('es');return false;" title="Spanish" class="link notranslate">Espanyol</a>
+    <a href="#" onclick="doGTranslate('es'); return false;" title="Spanish" class="link notranslate">Espanyol</a>
 </li>
 |
 <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('pt');return false;" title="Portuguese" class="link nturl notranslate">Portuguese</a>
+    <a href="#" onclick="doGTranslate('pt'); return false;" title="Portuguese" class="link nturl notranslate">Portuguese</a>
 </li>
 |
 <li style="position:relative;" class="menu-item menu-item-gtranslate">
-    <a href="#" onclick="doGTranslate('sw');return false;" title="Kiswahili" class="glink nturl notranslate">Swahili</a>
+    <a href="#" onclick="doGTranslate('sw'); return false;" title="Kiswahili" class="link nturl notranslate">Swahili</a>
 </li>
 </ul>
 </div>
