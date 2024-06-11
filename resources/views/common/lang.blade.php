03cc10998f49063f590e21e2b56f1e39377e45f9
Agaba Andrew <agabaandre@gmail.com>
2024-06-11 13:59:03 +0300

    default user langauge

--- resources/views/common/lang.blade.php
+++ resources/views/common/lang.blade.php
@@ -1,12 +1,12 @@
 <div id="google_translate_element" style="display: none;"></div>
     <select class="form-control select2" onchange="translateLanguage(this.value);" style="border:#FFF;" name="langauge">
-        <option value="en">English</option>
-        <option value="ar">Arabic</option>
-        <option value="fr">French</option>
-        <option  value="pt">Portuguese
+        <option value="en" {{ ('en' == $user->langauge) ? 'selected' : '' }}>English</option>
+        <option value="ar" {{ ('ar' == $user->langauge) ? 'selected' : '' }}>Arabic</option>
+        <option value="fr" {{ ('fr' == $user->langauge) ? 'selected' : '' }}>French</option>
+        <option  value="pt" {{ ('pt' == $user->langauge) ? 'selected' : '' }}>Portuguese
         </option>
-        <option value="es">Spanish</option>
-        <option value="sw">Swahili</option>
+        <option value="es" {{ ('es' == $user->langauge) ? 'selected' : '' }}>Spanish</option>
+        <option value="sw" {{ ('sw' == $user->langauge) ? 'selected' : '' }}>Swahili</option>
 
 
 </select>
