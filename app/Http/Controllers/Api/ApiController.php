2fb7709123150b4b861c03d8f5044b8ea55146da
Henry Nkuke <henricsanyu@gmail.com>
2024-08-11 14:49:37 +0300

    demo api server

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 #[
     OA\Server(url: 'http://localhost/knowhub', description: "Local Development server"),
     OA\Server(url: 'https://khub.africacdc.org/', description: "Production server"),
+    OA\Server(url: 'https://khub.africacdc.org/demo/', description: "Demo server"),
 ]
 
 //OA\SecurityScheme( securityScheme: 'jwt', type: "http", name: "Authorization", in: "header", scheme: "bearer",bearerFormat: "JWT"),
