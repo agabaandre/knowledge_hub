9793115ae324c024958951630d10022a08e574b2
Agaba Andrew <agabaandre@gmail.com>
2024-08-12 01:12:55 +0300

    pull

--- resources/views/partials/countries/dropdown.blade.php
+++ resources/views/partials/countries/dropdown.blade.php
@@ -1,5 +1,5 @@
 
-<select class="form-control {{ $class ?? 'select' }} text-left form-select" name="{{ $field ?? 'country_id' }}" {{ $required ?? '' }}>
+<select class="form-control {{ $class ?? }} text-left form-select" name="{{ $field ?? 'country_id' }}" {{ $required ?? '' }}>
     <option {{ (@$allfield)?'':'disabled' }} selected value="">{{ $allfield ?? 'Select' }}</option>
     @foreach ($countries as $country)
         <option 
