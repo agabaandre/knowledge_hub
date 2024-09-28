7162459120390b62bb48c0dc498f1bf701699767
Agaba Andrew <agabaandre@gmail.com>
2024-09-29 01:12:39 +0300

    added event type

---
+++ database/migrations/2024_09_28_221104_create_kpi_data.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateKpiViewIfNotExists extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Check if the view exists before creating it
+        DB::statement("
+            CREATE OR REPLACE VIEW kpi_data AS
+            SELECT 
+                `k`.`id` AS `kpi_id`,
+                SUM(`d`.`value`) AS `kpi_value`,
+                `d`.`period` AS `period`,
+                YEAR(CONCAT(`d`.`period`, '-01')) AS `period_year`,
+                MONTH(CONCAT(`d`.`period`, '-01')) AS `period_month`,
+                `k`.`name` AS `kpi_name`,
+                `k`.`subject_area` AS `subject_area_id`,
+                `k`.`description` AS `description`,
+                `k`.`frequency` AS `frequency`,
+                `c`.`name` AS `name`,
+                `c`.`color` AS `color`,
+                `c`.`id` AS `country_id`
+            FROM 
+                `knowledge_hub`.`data` `d`
+            JOIN 
+                `knowledge_hub`.`kpi` `k` ON `d`.`kpi_id` = `k`.`id`
+            JOIN 
+                `knowledge_hub`.`country` `c` ON `d`.`country_id` = `c`.`id`
+            GROUP BY 
+                `d`.`kpi_id`, `d`.`period`
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop the view if it exists
+        DB::statement("DROP VIEW IF EXISTS kpi_data");
+    }
+}
