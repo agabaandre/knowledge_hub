e3e6796bb16a198f069937659b36adca10e449b8
Agaba Andrew <agabaandre@gmail.com>
2024-09-29 01:38:20 +0300

    igration view

--- database/migrations/2024_09_28_221104_create_kpi_data.php
+++ database/migrations/2024_09_28_221104_create_kpi_data.php
@@ -12,30 +12,7 @@ class CreateKpiData extends Migration
      */
     public function up()
     {
-        DB::statement("
-            CREATE OR REPLACE VIEW kpi_view AS
-            SELECT 
-                `k`.`id` AS `kpi_id`,
-                SUM(`d`.`value`) AS `kpi_value`,
-                `d`.`period` AS `period`,
-                YEAR(CONCAT(`d`.`period`, '-01')) AS `period_year`,
-                MONTH(CONCAT(`d`.`period`, '-01')) AS `period_month`,
-                `k`.`name` AS `kpi_name`,
-                `k`.`subject_area` AS `subject_area_id`,
-                `k`.`description` AS `description`,
-                `k`.`frequency` AS `frequency`,
-                `c`.`name` AS `name`,
-                `c`.`color` AS `color`,
-                `c`.`id` AS `country_id`
-            FROM 
-                `knowledge_hub`.`data` `d`
-            JOIN 
-                `knowledge_hub`.`kpi` `k` ON `d`.`kpi_id` = `k`.`id`
-            JOIN 
-                `knowledge_hub`.`country` `c` ON `d`.`country_id` = `c`.`id`
-            GROUP BY 
-                `d`.`kpi_id`, `d`.`period`
-        ");
+        DB::statement("DROP VIEW IF EXISTS kpi_data");
     }
 
     /**
---
+++ database/migrations/2024_09_28_223701_create_kpi_data_view.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKpiDataView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kpi_data_view', function (Blueprint $table) {
+            DB::statement("
+            CREATE OR REPLACE VIEW kpi_view AS
+            SELECT 
+                `k`.`id` AS `kpi_id`,
+                SUM(`d`.`value`) AS `kpi_value`,
+                `d`.`period` AS `period`,
+                YEAR(CONCAT(`d`.`period`, '-01')) AS `period_year`,
+                MONTH(CONCAT(`d`.`period`, '-01')) AS `period_month`,
+                `k`.`name` AS `kpi_name`,
+                `k`.`subject_area` AS `subject_area_id`,
+                `k`.`description` AS `description`,
+                `k`.`frequency` AS `frequency`,
+                `c`.`name` AS `name`,
+                `c`.`color` AS `color`,
+                `c`.`id` AS `country_id`
+            FROM 
+                `knowledge_hub`.`data` `d`
+            JOIN 
+                `knowledge_hub`.`kpi` `k` ON `d`.`kpi_id` = `k`.`id`
+            JOIN 
+                `knowledge_hub`.`country` `c` ON `d`.`country_id` = `c`.`id`
+            GROUP BY 
+                `d`.`kpi_id`, `d`.`period`
+        ");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("DROP VIEW IF EXISTS kpi_data");
+    }
+}
