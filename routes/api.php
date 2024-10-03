a1599b43fda6947ffb28151fb331feb3132efd4c
Henry Nkuke <henricsanyu@gmail.com>
2024-10-03 19:37:41 +0300

    favourites on api

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ Route::group(['middleware' => 'auth:api','prefix'=>"publications"],function(){
     Route::get("/published",[PublicationsApiController::class,"my_publications"]);
     Route::post("/comment",[PublicationsApiController::class,"comment"]);
     Route::get("/favourites",[PublicationsApiController::class,"favourites"]);
-    Route::get("/add_favourite/{id}",[PublicationsApiController::class,"add_favourite"]);
+    Route::get("/add_favourite",[PublicationsApiController::class,"add_favourite"]);
 });
 
 Route::get("forums",[ForumsApiController::class,"index"]);
