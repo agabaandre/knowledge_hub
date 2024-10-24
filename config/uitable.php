30e5046da95314bc208aa9c560934096a4b97ac4
Henry Nkuke <henricsanyu@gmail.com>
2024-10-24 22:46:43 +0300

    uitable config cleanup

--- config/uitable.php
+++ config/uitable.php
@@ -7,7 +7,7 @@ $db_conf["user"]     = env('DB_USERNAME'); // username
 $db_conf["password"] = env('DB_PASSWORD'); // password
 $db_conf["database"] = env('DB_DATABASE');
 
-define("PHPGRID_LIBPATH",base_path("libs/phpgrid/"));
+//define("PHPGRID_LIBPATH",base_path("libs/phpgrid/"));
 
 return [
     "db_config"=>$db_conf,
