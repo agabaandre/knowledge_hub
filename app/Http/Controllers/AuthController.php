33290b0488085f76dd9cef1fafe0c0486f713fd1
Henry Nkuke <henricsanyu@gmail.com>
2024-11-14 09:42:00 +0300

    social login fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -150,6 +150,14 @@ class AuthController extends Controller
          // Convert the GoogleUser object to a standard object
          $user = json_decode(json_encode($socialUser));
 
+         if($this->usersRepo->find_by_email($user->user->email,true)){
+
+            $data['alert_class'] = 'danger';
+            $data['message']     = "User with this email exists and can login with username and password";
+            $data['status']      = 200;
+            return redirect('/login');
+        }
+
          $user =$this->socialLoginService->googleCallback($user);
          Auth::login($user);
          return redirect('/account');
