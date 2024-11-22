587540433da5c7dd937ba9a00c65cc5ba8d228fe
nkukehenry <henricsanyu@gmail.com>
2024-11-23 00:24:51 +0300

    photo external

--- database/migrations/2024_11_13_191849_add_dis_image_external_to_users_table.php
+++ database/migrations/2024_11_13_191849_add_dis_image_external_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddIsImageExternalToUsersTable extends Migration
+class AddIsPhotoExternalToUsersTable extends Migration
 {
     /**
      * Run the migrations.
