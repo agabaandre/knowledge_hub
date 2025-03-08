22f35cdf7c5211c09c73e4da563bd2ff9bfa7cfe
Agaba Andre <agabaandre@gmail.com>
2025-03-09 00:00:16 +0300

    Update show.blade.php

--- resources/views/publications/show.blade.php
+++ resources/views/publications/show.blade.php
@@ -63,10 +63,11 @@
                                         <label class="text-muted">Likes</label>
                                         <span
                                             class="px-2 py-1 ft-medium medium text-light theme-bg rounded mr-2">{{ count($publication->favourited) }}
-                                            Likes</span>
+                                            Likes
+                                        </span>
                                     </div>
                                     <div class="details ft-medium col-sm-12 col-lg-4">
-                                    <span class="px-2 py-1 text-light theme-bg rounded mr-2">{{ !$publication->is_version ? $publication->sub_theme->description ?? '' : 'Version ' . $publication->version_no }}</span>
+                                    <span class="text-light theme-bg rouded">{{ !$publication->is_version ? $publication->sub_theme->description ?? '' : 'Version ' . $publication->version_no }}</span>
                                     </div>
                                     @if (count($publication->favourited) > 0)
                                         @php
