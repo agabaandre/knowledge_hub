31e02061bfd436dbbdf463dcdf6f067462275a61
Agaba Andre <agabaandre@gmail.com>
2025-06-26 12:08:45 +0300

    updated spotlight

--- resources/views/home/partials/top_categories.blade.php
+++ resources/views/home/partials/top_categories.blade.php
@@ -1,33 +1,35 @@
+			<!-- ================================ Tag Award ================================ -->
+			<section class="py-3" style="margin-top: -5%;" >
+			
+					<div class="row justify-content-center">
+						<div class="col-lg-12 col-md-12 col-sm-12">
+							<div class="crp_box fl_color ovr_top">
+								<div class="row align-items-center">
+								
+								@foreach($categories as $category)
+										
+								<div class="col-xl-3 col-lg-3 col-md-3 col-sm-12">
+								<a href="{{url($category['link'])}}">
+										<div class="dro_140">
+											<div class="dro_141 de">
+												<i class="{{$category['icon']}}"></i>
+												
+											</div>
+											<div class="dro_142">
+												<h6>{{$category['title']}}</h6>
+												<!-- <p>{{$category['stats']}} Records</p> -->
+											</div>
+										</div>
+								</a>
+									</div>
+								@endforeach
 
+								</div>
 
-<!-- ================================ Categorization Section ================================ -->
-<section class="py-5 bg-light" id="categorization">
-    <div class="container">
-        <!-- Section Header -->
-        <div class="row mb-4">
-            <div class="col text-center">
-                <h3 class="fw-bold text-primary">Explore Key Sections</h3>
-                <p class="text-muted mb-0">Browse health topics, contributors, coverage, and forums to navigate the platform efficiently.</p>
-            </div>
-        </div>
-
-        <!-- Category Cards -->
-        <div class="row g-4">
-            @foreach($categories as $category)
-                <div class="col-12 col-sm-6 col-md-3">
-                    <a href="{{ url($category['link']) }}" class="text-decoration-none">
-                        <div class="card category-card text-center p-4">
-                            <div class="category-icon text-primary">
-                                <i class="{{ $category['icon'] }} fa-2x"></i>
-                            </div>
-                            <h6 class="text-dark">{{ $category['title'] }}</h6>
-                            @if(isset($category['description']))
-                                <p class="mt-2">{{ Str::limit($category['description'], 80) }}</p>
-                            @endif
-                        </div>
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    </div>
-</section>
+							</div>
+						</div>
+					</div>
+				
+				
+			</section>
+		
