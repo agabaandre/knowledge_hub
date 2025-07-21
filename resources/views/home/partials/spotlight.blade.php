4d944ddb49748d5ed2a8ebcd4f0ed3f58135031b
Agaba Andre <agabaandre@gmail.com>
2025-07-21 21:05:30 +0300

    new updates

--- resources/views/home/partials/spotlight.blade.php
+++ resources/views/home/partials/spotlight.blade.php
@@ -1,36 +1,33 @@
-<div class="spotlight px-3 py-3 custom-bg"  style="background: linear-gradient(var(--theme-color-primary), rgba(0, 0, 0, 0.4)) !important;" style="background-color: linear-gradient(var(--theme-color-primary), rgba(0, 0, 0, 0.4)) !important; background-image: url({{settings()->spotlight_banner }});">
-<form action="{{ url('records/search') }}" class="filters" style="min-width: 70%;">
-							<div class="row no-gutters bg-white rounded search-form" id="simple_search">
-									<div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
-										<div class="form-group mb-0 position-relative main_search">
-											<input type="text" class="form-control left-ico autocomplete term main-search" name="term" value="{{ @old('term') }}" placeholder="Type Keywords" />
-										</div>
-									</div>
-								
-									<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 bg-show">
-										<div class="form-group mb-0 position-relative">
-											<button class="btn full-width  theme-bg text-white fs-md py-3" type="submit"><i class="fa fa-magnifying-glass"></i>Search</button>
-										</div>
-									</div>
-
-								</div>
-                @include('partials.search.advanced_search')
-
-                
-
-                
-            <div class="col-md-4 sm-show mt-1">
-            <button class="btn full-width  theme-bg text-white fs-md py-3" type="submit"><i class="fa fa-magnifying-glass"></i>Search</button>
+<div class="spotlight px-3 py-3 custom-bg"
+     style="background: linear-gradient(var(--theme-color-primary), rgba(0, 0, 0, 0.4)) !important; background-image: url('{{ settings()->spotlight_banner }}');">
+    <form action="{{ url('records/search') }}" class="filters" style="min-width: 70%;" role="search" aria-label="Search records">
+        <div class="row no-gutters bg-white rounded search-form" id="simple_search">
+            <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12 col-12">
+                <div class="form-group mb-0 position-relative main_search">
+                    <label for="main-search" class="sr-only">Search Keywords</label>
+                    <input type="text" id="main-search" class="form-control left-ico autocomplete term main-search"
+                           name="term" value="{{ old('term') }}" placeholder="Type Keywords" />
+                </div>
             </div>
-	</form>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 bg-show">
+                <div class="form-group mb-0 position-relative">
+                    <button class="btn full-width theme-bg text-white fs-md py-3" type="submit">
+                        <i class="fa fa-magnifying-glass"></i> Search
+                    </button>
+                </div>
+            </div>
+        </div>
+        @include('partials.search.advanced_search')
+        <div class="col-md-4 sm-show mt-1 d-md-none">
+            <button class="btn full-width theme-bg text-white fs-md py-3" type="submit">
+                <i class="fa fa-magnifying-glass"></i> Search
+            </button>
+        </div>
+    </form>
 
-<div  class="row spot-row col-sm-12 d-flex align-items-center">
+    <div class="row spot-row col-sm-12 d-flex align-items-center">
+        @include('home.partials.theme_tabs')
+    </div>
 
-      @include('home.partials.theme_tabs') 
-
-</div>
-
-<div  class="spot-row">
-</div>
-
+    <div class="spot-row"></div>
 </div>
