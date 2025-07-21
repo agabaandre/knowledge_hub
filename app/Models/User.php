3d7efae5df89016189dd7e813104ca5a729e0566
nkukehenry <henricsanyu@gmail.com>
2025-07-22 00:01:09 +0300

    default memo to draft

--- app/Models/User.php
+++ app/Models/User.php
@@ -69,7 +69,7 @@ class User extends Authenticatable
      }
 
      public function getPhotottribute(){
-        return ($this->is_photo_external)?$this->photo:user_profile_photo($this->photo);
+        return (intval($this->is_photo_external)==1)?$this->photo:user_profile_photo($this->photo);
      }
 
      public function access_level(){
