51a1d71df5ab8b3dc3819d02a981bf8281e81406
Agaba Andre <agabaandre@gmail.com>
2025-07-22 16:07:48 +0300

    photo display

--- resources/views/layouts/partials/header_resources.blade.php
+++ resources/views/layouts/partials/header_resources.blade.php
@@ -200,7 +200,7 @@
 
 
 <!--Start of Tawk.to Script-->
-<script type="text/javascript">
+{{-- <script type="text/javascript">
     var Tawk_API = Tawk_API || {},
         Tawk_LoadStart = new Date();
     (function() {
@@ -212,5 +212,5 @@
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
     })();
-</script>
+</script> --}}
 <!--End of Tawk.to Script-->
