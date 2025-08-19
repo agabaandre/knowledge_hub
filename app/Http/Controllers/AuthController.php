a5438d21ad03c69d95731bd8af0a97b927859d3f
Agaba Andre <agabaandre@gmail.com>
2025-08-19 10:42:25 +0300

    microsoft update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -129,7 +129,7 @@ class AuthController extends Controller
 
         $user_exists = $this->usersRepo->find_by_email($user->user->mail);
 
-        dd($user_exists);
+        //dd($user_exists);
 
          // Check if the user already exists in the database
          // If not, create a new user using the social login service
