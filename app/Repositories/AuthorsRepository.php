6b5686ac8e2cd2f051ee8131fab4339fe78964ef
Agaba Andre <agabaandre@gmail.com>
2026-04-02 12:56:20 +0300

    updated user profile and comms

--- app/Repositories/AuthorsRepository.php
+++ app/Repositories/AuthorsRepository.php
@@ -14,17 +14,23 @@ class AuthorsRepository extends SharedRepo{
         $forumEngagementSubquery = DB::table('forum_engagements')
             ->select('user_id', DB::raw('SUM(forum_posts + forum_comments) as forum_engagement_total'))
             ->groupBy('user_id');
+        $publicationCountSubquery = DB::table('publication')
+            ->select('author_id', DB::raw('COUNT(*) as publications_count'))
+            ->groupBy('author_id');
 
         $authors = Author::query()
             ->with(['user.country', 'user.badges.badgeType', 'user.communities', 'publications'])
-            ->withCount('publications')
             ->leftJoin('users as author_users', 'author_users.author_id', '=', 'author.id')
+            ->leftJoinSub($publicationCountSubquery, 'publication_totals', function ($join) {
+                $join->on('publication_totals.author_id', '=', 'author.id');
+            })
             ->leftJoinSub($forumEngagementSubquery, 'forum_totals', function ($join) {
                 $join->on('forum_totals.user_id', '=', 'author_users.id');
             })
             ->select('author.*')
+            ->selectRaw('COALESCE(publication_totals.publications_count, 0) as publications_count')
             ->selectRaw('COALESCE(forum_totals.forum_engagement_total, 0) as forum_engagement_total')
-            ->selectRaw('(COALESCE(publications_count, 0) + COALESCE(forum_totals.forum_engagement_total, 0)) as total_contributions');
+            ->selectRaw('(COALESCE(publication_totals.publications_count, 0) + COALESCE(forum_totals.forum_engagement_total, 0)) as total_contributions');
 
         if($request->term) {
             $searchTerm = '%'.$request->term.'%';
